0ceeb788f32a3cbe069eebd0864f9cbe272d9a36
Maximilian Coreachin <maxcorechin@Maximilians-MacBook-Air.local>
2024-04-06 21:22:21 -0400

    Initial commit

---
+++ src/test/java/com/example/flowerstorewebappms/FlowerStoreWebAppMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flowerstorewebappms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowerStoreWebAppMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
