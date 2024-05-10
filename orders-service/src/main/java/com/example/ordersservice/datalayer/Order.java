82b484db63cfa7191ed5d9bbf60b612d1f892edf
Maximilian Coreachin <coreachinmax@gmail.com>
2024-05-09 21:07:21 -0400

    Revert "Remove orphanRemoval attribute from OrderItem relationship"
    This reverts commit ae785fd1b862f49d35d5e8336def96d709b41f7d.

--- orders-service/src/main/java/com/example/ordersservice/datalayer/Order.java
+++ orders-service/src/main/java/com/example/ordersservice/datalayer/Order.java
@@ -32,7 +32,7 @@ public class Order {
     @Valid
     private CustomerDetails customerDetails;
 
-    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER, orphanRemoval = true)
     @JoinColumn(name = "order_id")
     @Valid
     private Set<OrderItem> items;
